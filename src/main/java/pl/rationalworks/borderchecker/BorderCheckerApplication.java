3f1c9a3c101b29dadd388642ec868bad9e2125da
winciu <winciu@gazeta.pl>
2023-07-30 23:36:39 +0200

    added initial project structure

---
+++ src/main/java/pl/rationalworks/borderchecker/BorderCheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.borderchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rationalworks/borderchecker/BorderCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rationalworks.borderchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
